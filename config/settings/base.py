9fec082341eed6ac7dd6382e48b146cf3863ea6f
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-13 17:28:37 -0400

    add django extensions to local

--- config/settings/base.py
+++ config/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     # Project Apps
     "apps.users",
     "apps.pages",
-    "apps.management"
+    "apps.management",
     # Django Apps
     "django.contrib.admin",
     "django.contrib.auth",
