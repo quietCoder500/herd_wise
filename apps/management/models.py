4c9d9cf690900bbd52a21f3025d12b79c5c3a296
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-13 16:20:50 -0400

    add base animal types to animal model

--- apps/management/models.py
+++ apps/management/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Farm(models.Model):
+    pass
+
+class AnimalGroup(models.Model):
+    pass
+
+class Animal(models.Model):
+    ANIMAL_CATEGORY = [
+        ("MC", "Market Chicken"),
+        ("EC", "Exhibition Chicken"),
+        ("ED", "Exhibition Duck"),
+        ("MD", "Market Duck"),
+        ("EG", "Exhibition Goose"),
+        ("MG", "Market Goose"),
+        ("ET", "Exhibition Turkey"),
+        ("MT", "Market Turkey"),
+        ("HGF", "Helmeted Guinea Fowl"),
+        ("BS", "Breeding Swine"),
+        ("MH", "Market Hog"),
+        ("MB", "Market Beef"),
+        ("MBF", "Beef, Feeder"),
+        ("MDF", "Dairy Beef Feeder"),
+        ("BB", "Beef, Breeder"),
+        ("RB", "Rabbit, Breeder"),
+        ("MR", "Market Rabbit"),
+        ("RP", "Pet Rabbit"),
+        ("DCF", "Dairy Calf"),
+        ("DYH", "Dairy Yearling Heifer"),
+        ("DC", "Dairy Cow"),
+        ("DG", "Dairy Goat"),
+        ("MG", "Market Goat"),
+        ("SG", "Specialty Goat"),
+        ("HO", "Horse"),
+        ("LA", "Llama"),
+        ("AL", "Alpaca"),
+        ("ML", "Market Lamb"),
+        ("BS", "Breed Sheep"),
+    ]
+    category = models.CharField(max_length=3, null=False, blank=False, choices=ANIMAL_CATEGORY)
+    breed = models.CharField(max_length=200, null=True)
+    date_of_birth = models.DateField(null=True)
+    age = models.IntegerField(null=True)
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+    tag_id = models.UUIDField(verbose_name="Tag UUID", unique=True, null=True, blank=False)
+
+    group = models.ForeignKey(AnimalGroup, verbose_name="Belongs to group: ", on_delete=models.CASCADE)
+
