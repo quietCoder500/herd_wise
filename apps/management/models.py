8734621214ed2d6313f1efb04136b20653aa9938
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-13 21:54:04 -0400

    create public facing uuids for models and redo migrations

--- apps/management/models.py
+++ apps/management/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
+from django_extensions.db.fields import ShortUUIDField
 from apps.users.models import User
 
+import shortuuid
+import uuid
+
 class Farm(models.Model):
+    name = models.CharField(verbose_name="name",max_length=100)
+    public_id = ShortUUIDField(unique=True, editable=False, default=shortuuid.uuid)
     users = models.ManyToManyField(User)
 
+
 class AnimalGroup(models.Model):
-    name = models.CharField(max_length=200)
+    NAMING_SCHEMA = [
+        ("CI", "<Animal Category> - <Animal Number>"),
+        ("AN", "<Animal Name>"),
+        ("BI", "<Animal Breed> - <Animal Number>")
+    ]
+    name = models.CharField(max_length=100)
+    public_id = ShortUUIDField(unique=True, editable=False, default=shortuuid.uuid)
     farm = models.ForeignKey(Farm, on_delete=models.PROTECT, related_name="animal_groups")
+    animal_naming_schema = models.CharField(max_length=2, choices=NAMING_SCHEMA)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(AnimalGroup, self).save(*args, **kwargs)
+        
+
 
 class Animal(models.Model):
     ANIMAL_CATEGORY = [
@@ -40,14 +62,17 @@ class Animal(models.Model):
         ("ML", "Market Lamb"),
         ("BS", "Breed Sheep"),
     ]
+
+    public_id = ShortUUIDField(unique=True, editable=False, default=shortuuid.uuid)
+
     category = models.CharField(max_length=3, null=False, blank=False, choices=ANIMAL_CATEGORY)
     breed = models.CharField(max_length=200, null=True, blank=True)
 
     date_of_birth = models.DateField(null=True, blank=True)
-    age = models.IntegerField(null=True, blank= True)
+    age = models.IntegerField(null=True, blank=True)
 
     name = models.CharField(max_length=50, blank=True, null=True)
-    tag_id = models.UUIDField(verbose_name="Tag UUID", unique=True, null=True, blank=True)
+    tag_id = models.UUIDField(verbose_name="Tag UUID", unique=True, null=True, blank=True, default=uuid.uuid4)
 
     group = models.ForeignKey(AnimalGroup, verbose_name="Belongs to group: ", related_name="animals", on_delete=models.PROTECT)
 
