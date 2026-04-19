6b2f328942df3507d45ff0cd0321867b26997862
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-19 15:33:21 -0400

    add admin for models

--- apps/records/admin.py
+++ apps/records/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from apps.records.models import Note, MedicalRecord, WeightRecord
 # Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MedicalRecord)
+class MedicalRecordAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WeightRecord)
+class WeightRecordAdmin(admin.ModelAdmin):
+    pass
