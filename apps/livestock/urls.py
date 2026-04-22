1964d1df26fad72432523efa1410bc04a1fdc3b1
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-22 15:58:04 -0400

    add the rest of the basic model views for livestock

--- apps/livestock/urls.py
+++ apps/livestock/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from apps.livestock import views
 
 urlpatterns = [
-    path("farm/", views.farm_list),
+    path("farms/", views.farm_list),
     path("farm/<str:short_uuid>/", views.farm_detail),
+    path("animal_groups/<str:farm_short_uuid>/", views.animal_group_list),
+    path("animal_group/<str:short_uuid>/", views.animal_group_detail),
+    path("animals/<str:animal_group_short_uuid>/", views.animal_list),
+    path("animal/<str:short_uuid>/", views.animal_detail),
 ]
--- apps/livestock/views.py
+++ apps/livestock/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, get_list_or_404, render
-from apps.livestock.models import Farm
+from apps.livestock.models import Farm, Animal, AnimalGroup
 
 
 @login_required
@@ -18,14 +18,22 @@ def farm_detail(request, short_uuid):
     obj = get_object_or_404(Farm, public_id=short_uuid)
     return render(request, "livestock/farm/detailed.html", context={"farm": obj})
 
-def animal_group_list():
-    pass
+@login_required
+def animal_group_list(request, farm_short_uuid):
+    animal_groups = get_list_or_404(AnimalGroup, farm__users=request.user, farm__public_id=farm_short_uuid)
+    return render(request, "livestock/animal_group/list.html", context={"animal_groups": animal_groups})
 
-def animal_group_detail():
-    pass
+@login_required
+def animal_group_detail(request, short_uuid):
+    obj = get_object_or_404(AnimalGroup, public_id=short_uuid)
+    return render(request, "livestock/animal_group/detailed.html")
 
-def animal_list():
-    pass
+@login_required
+def animal_list(request, animal_group_short_uuid):
+    animals = get_list_or_404(Animal, group__public_id=animal_group_short_uuid)
+    return render(request, "livestock/animal/list.html", context={"animals": animals})
 
-def animal_detail():
-    pass
+@login_required
+def animal_detail(request, short_uuid):
+    obj = get_object_or_404(Animal, public_id=short_uuid)
+    return render(request, "livestock/animal/detailed.html", context={"animal": obj})
