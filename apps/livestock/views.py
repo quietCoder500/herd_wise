dff2393db51479805f043b68796886476a7e265d
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-22 08:09:20 -0400

    add basic views and fix template naming

--- apps/livestock/views.py
+++ apps/livestock/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def farm_list():
+    pass
+
+def farm_detail():
+    pass
+
+def animal_group_list():
+    pass
+
+def animal_group_detail():
+    pass
+
+def animal_list():
+    pass
+
+def animal_detail():
+    pass
