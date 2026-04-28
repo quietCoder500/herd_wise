820bc3c0184d9b8f8d78936c8b6ef5e5c92bdbdb
quietCoder500 <raspberrypisms1@gmail.com>
2026-04-28 13:21:58 -0400

    add sentry (irrelevant but I wanted to)

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import include, path
 from apps.users.views import StandardSignupView
 
+
 urlpatterns = [
     path("", include("apps.pages.urls")),
     path("admin/", admin.site.urls),
