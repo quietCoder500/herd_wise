e026505a2119e4fe52fcea43b3677ca9777c6898
quietCoder500 <raspberrypisms1@gmail.com>
2026-05-08 16:08:40 -0400

    ruff formatting

--- apps/livestock/views.py
+++ apps/livestock/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from apps.livestock.models import Farm, AnimalGroup, Animal
 
 
 def dashboard_home(request):
-    return render(request, "livestock/dashboard/index.html")
+    farm_objs = Farm.objects.get()
+    miller_columns = [
+        {"id": 1, "header": "", "models": farm_objs},
+    ]
+    return render(
+        request, "livestock/dashboard/index.html", context={"columns": miller_columns}
+    )
