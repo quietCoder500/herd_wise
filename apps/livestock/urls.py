b2ccb550d4d0c0fb3d7adf25a6d39a9aa7bf9110
quietCoder500 <raspberrypisms1@gmail.com>
2026-05-23 15:09:47 -0400

    add detail view base

--- apps/livestock/urls.py
+++ apps/livestock/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("", views.index, name="farms-col"),
     path("farm/<str:farm_pub_id>", views.farm_col, name="farm"),
     path("group/<str:group_pub_id>", views.group_col, name="group"),
-    path("animal/<str:animal_pub_id>", views.animal_col, name="animal")
+    path("report/<str:rep_type>/<str:animal_pub_id>", views.report_detail_col, name="animal")
 ]
--- apps/livestock/views.py
+++ apps/livestock/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from apps.livestock.models import Farm, AnimalGroup, Animal
 from utils.lib import AlpineTemplateResponse
 
 # TODO: Convert all filters to objects or 404
 
+#
+# Main page
+#
 @login_required
 def index(request):
     """Renders the column containing all farms related to the user"""
@@ -13,6 +16,9 @@ def index(request):
         request, "livestock/dashboard/index.html", context={"farms": farm_objs}
     )
 
+#
+# Miller columns
+#
 @login_required
 def farm_col(request, farm_pub_id):
     """Renders the column containing all animal groups related to this farm"""
@@ -25,8 +31,14 @@ def group_col(request, group_pub_id):
     return AlpineTemplateResponse(request, "livestock/dashboard/index.html", context)
 
 @login_required
-def animal_col(request, animal_pub_id):
-    context = {"animals": Animal.objects.filter(public_id=animal_pub_id)}
+def options_col(request, group_pub_id):
+    context = {"animals": Animal.objects.filter(group__public_id=group_pub_id, group__farm__users=request.user)}
     return AlpineTemplateResponse(request, "livestock/dashboard/index.html", context)
 
-
+#
+# Final tier
+#
+@login_required
+def report_detail_col(request, rep_type, animal_pub_id):
+    context = {"animal": get_object_or_404(Animal, public_id=animal_pub_id, group__farm__users=request.user)}
+    return render(request, "livestock/dashboard/segments/_report_detail.html", context=context)
